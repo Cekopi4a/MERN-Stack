c3a3b4dcad12b7f5926a0c9fe6d9bf47c4ce3773
Cekopi4a <106926784+Cekopi4a@users.noreply.github.com>
2024-07-01 00:01:07 +0300

    Update main.jsx

--- mern/client/src/main.jsx
+++ mern/client/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { BrowserRouter} from 'react-router-dom'
-import { AuthContextProvider } from './context/AuthContext.jsx'
+import { AuthContextProvider } from './context/аuthContext.jsx'
 import { CartProvider } from './context/cartContext.jsx'
 import { OrderProvider} from './context/OrderContext.jsx'
 import './i18n'; // Инициализация на i18n
